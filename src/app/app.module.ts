7a8f62b23baca001c5a2334483bd50a196b6fff3
Halyna Tesliuk <halyna_tesliuk@epam.com>
2022-10-08 18:34:40 +0300

    added login component with validation of the form, module and added LoginModule to AppComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { CoreModule } from "./core/core.module";
+import {LoginModule} from "./pages/login/login.module";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  login = '';
+  password = ''
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+
+  }
+
+  authorization(event: any): void {
+    this.cleanErrorMessage();
+    event.preventDefault();
+    this.checkInputtedData();
+  }
+
+  checkInputtedData(): void {
+    const errors = [];
+    const emailRegExp = /^[A-Za-z0-9_\-.]+[@][a-z]+[.][a-z]{3}$/;
+    const loginRegExp = /^[A-Za-z0-9_\-.@!]{8,}/;
+    if (!emailRegExp.test(this.login)) {
+      errors[0] = new String('Not valid login! An example of login: test@gmail.com');
+    }
+    if (!loginRegExp.test(this.password)) {
+      errors[1] = new String('Not valid password! Required min 8 symbols');
+    }
+    if (errors.length > 0) {
+      let ulElement = <HTMLVideoElement>document.querySelector('#message');
+      ulElement.innerText = "Error!";
+      errors.forEach(el => {
+        let liElement = document.createElement('li');
+        console.log(`${el}`);
+        liElement.innerHTML = `${el}`;
+        ulElement.appendChild(liElement);
+      });
+    } else {
+      let message = <HTMLVideoElement>document.querySelector('#message');
+      message.innerText = 'successful';
+    }
+  }
+
+
+  cleanErrorMessage(): void {
+    let message = <HTMLVideoElement>document.querySelector('#message');
+    message.innerText = '';
+    message.innerHTML = '';
+  }
 }
