89a3df94db726845fd41c41089244ae57a8db27c
Halyna Tesliuk <halyna_tesliuk@epam.com>
2022-10-08 20:02:28 +0300

    added MainComponent, MainModule and MainRoutingModule

---
+++ src/app/pages/main/main-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Routes, RouterModule} from "@angular/router";
+import {MainComponent} from "./main.component";
+
+const routes: Routes = [
+  {path: '', component: MainComponent}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule, [RouterModule.forChild(routes)],
+  ],
+  exports: [RouterModule]
+})
+export class MainRoutingModule { }
