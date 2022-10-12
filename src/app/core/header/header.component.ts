72d2c2393314837c710e04921bdb5081677318bc
Halyna Tesliuk <halyna_tesliuk@epam.com>
2022-10-12 18:08:55 +0300

    added implementation of "header" component

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../shared/services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  getUserAuthentication() {
+    return this.authService.isAuthenticated();
+  }
+
+  getUserInfo() {
+    return localStorage.getItem('userLogin');
+  }
+
+  logout() {
+  this.authService.logout();
+  this.router.navigate(['login'])
+  }
 }
